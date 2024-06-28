218b16f5610b62a61cdd6b1a891c3d0d2a4e24e4
Ali Küçükkaya <preoyuncu@hotmail.com>
2024-06-28 14:04:07 +0300

    .

--- ArcadiaFansub.API/Controllers/UserController.cs
+++ ArcadiaFansub.API/Controllers/UserController.cs
@@ -4,51 +4,51 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ArcadiaFansub.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController(UserHandler userHandler, UserAuthentication authHandler) : ControllerBase
-    {
-        [HttpPost("Login")]
-        public async Task<IActionResult> Login([FromBody] UserLoginRequest loginRequest, CancellationToken cancellationToken)
-        {
-            return await (userHandler.Login(loginRequest, cancellationToken)) is { } result ? Ok(result) : BadRequest(); ;
-        }
-        [HttpPost("Register")]
-        public async Task<IActionResult> Register([FromBody] CreateNewUserRequest registerRequest, CancellationToken cancellationToken)
-        {
-            if (registerRequest != null)
-            {
-                return Ok(await userHandler.CreateUser(registerRequest, cancellationToken));
-            }
-            else
-            {
-                return BadRequest();
-            }
-        }
-        [HttpPost("IsAdmin")]
-        public async Task<IActionResult> IsAdminAuthenticate([FromBody] UserAuthRequest request)
-        {
-            return (await authHandler.IsAdmin(request.UserToken)) is { } result ? Ok(result) : BadRequest();
-        }
-        [HttpPost("AuthUser")]
-        public async Task<IActionResult> AuthUser([FromBody] UserAuthRequest request)
-        {
-            return (await authHandler.AuthUser(request.UserToken)) is { } result ? Ok(result) : BadRequest();
-        }
-        [HttpPost("GetUserById")]
-        public async Task<IActionResult> GetUserById([FromBody] UserRequest userName, CancellationToken cancellationToken)
-        {
-            return await (userHandler.GetUserById(userName.UserName, cancellationToken)) is { } result ? Ok(result) : BadRequest();
-        }
-        [HttpPost("ResetUser")]
-        public async Task<IActionResult> ResetUser([FromBody] UserAuthRequest userRequest, CancellationToken cancellationToken)
-        {
-            return await (userHandler.ResetUser(userRequest.UserToken, cancellationToken)) is { } result ? Ok(result) : BadRequest();
-        }
-        [HttpPost("GetUserBySearch/{param}")]
-        public async Task<IActionResult> GetUserBySearch(string param, CancellationToken cancellationToken)
-        {
-            return await (userHandler.SearchUser(param, cancellationToken)) is { } result ? Ok(result) : NotFound();
-        }
-    }
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController(UserHandler userHandler, UserAuthentication authHandler) : ControllerBase
+	{
+		[HttpPost("Login")]
+		public async Task<IActionResult> Login([FromBody] UserLoginRequest loginRequest, CancellationToken cancellationToken)
+		{
+			return await (userHandler.Login(loginRequest, cancellationToken)) is { } result ? Ok(result) : BadRequest();
+		}
+		[HttpPost("Register")]
+		public async Task<IActionResult> Register([FromBody] CreateNewUserRequest registerRequest, CancellationToken cancellationToken)
+		{
+			if (registerRequest != null)
+			{
+				return Ok(await userHandler.CreateUser(registerRequest, cancellationToken));
+			}
+			else
+			{
+				return BadRequest();
+			}
+		}
+		[HttpPost("IsAdmin")]
+		public async Task<IActionResult> IsAdminAuthenticate([FromBody] UserAuthRequest request)
+		{
+			return (await authHandler.IsAdmin(request.UserToken)) is { } result ? Ok(result) : BadRequest();
+		}
+		[HttpPost("AuthUser")]
+		public async Task<IActionResult> AuthUser([FromBody] UserAuthRequest request)
+		{
+			return (await authHandler.AuthUser(request.UserToken)) is { } result ? Ok(result) : BadRequest();
+		}
+		[HttpPost("GetUserById")]
+		public async Task<IActionResult> GetUserById([FromBody] UserRequest userName, CancellationToken cancellationToken)
+		{
+			return await (userHandler.GetUserById(userName.UserName, cancellationToken)) is { } result ? Ok(result) : BadRequest();
+		}
+		[HttpPost("ResetUser")]
+		public async Task<IActionResult> ResetUser([FromBody] UserAuthRequest userRequest, CancellationToken cancellationToken)
+		{
+			return await (userHandler.ResetUser(userRequest.UserToken, cancellationToken)) is { } result ? Ok(result) : BadRequest();
+		}
+		[HttpPost("GetUserBySearch/{param}")]
+		public async Task<IActionResult> GetUserBySearch(string param, CancellationToken cancellationToken)
+		{
+			return await (userHandler.SearchUser(param, cancellationToken)) is { } result ? Ok(result) : NotFound();
+		}
+	}
 }
